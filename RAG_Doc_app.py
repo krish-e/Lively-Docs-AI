8405f88f87b824edaa24e70d1bcdd3e06a2df5e6
Kirishee Gunasekaran <krisheegunasekaran@gmail.com>
2024-04-02 13:55:43 +0530

    Update RAG_Doc_app.py

--- RAG_Doc_app.py
+++ RAG_Doc_app.py
@@ -22,42 +22,31 @@ Context: {context}
     
 Question: {question}
 """
-
 prompt = ChatPromptTemplate.from_template(template)
 
-
-# Splitting Book into small chunks
 # Replace file_path with the URL fo your document(local or online). Except google drive
 file_path = "https://pdfobject.com/pdf/sample.pdf"
 
+# Splitting Book into small chunks
 loader = PyPDFLoader(file_path)
 pages = loader.load_and_split()
 pages
 
 # Loading book to the vector store(local memory)
 embeddings = OpenAIEmbeddings() 
-
 vector_store = DocArrayInMemorySearch.from_documents(pages, embeddings)
 
 setup = RunnableParallel(context= vector_store.as_retriever(), question=RunnablePassthrough())
 chain = setup | prompt | model | parser
 
 exit_keywords = ['nothing', 'no', 'exit']
+
 print("\nLively Docs AI")
 question = input("Ask any question based on your document content (reply 'exit' to end session): \n")
 while True:
-    # question = "Which policy initiatives have made an impact in providing financial and income support to farmers?"
     if not question.lower() in exit_keywords:
         print(chain.invoke(question))
     else:
         break
     question = input("\nNext Question?\n")
     
-
-
-
-"""
-Lively Docs AI is a Command Line Interface (CLI) tool designed to enhance document understanding through AI-powered question answering. 
-Users can provide URL of  PDF documents (local or online sources except Google Drive). Once uploaded, they can ask questions related to 
-the content of the document, and the AI model, powered by OpenAI API, will generate answers using the Retrieve and Generate (RAG) mechanism.
-"""
